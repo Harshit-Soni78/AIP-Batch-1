55b9b8600a9aa77d509f9680868d5c7e17e05d9a
Harshit Soni <harshit11664@gmail.com>
2025-02-19 11:18:45 +0530

    Done Day-2

---
+++ Day-2/basic_chatbot.py
@@ -0,0 +1,10 @@
+from nltk.chat.util import Chat, reflections
+
+pairs = [
+    ["hi|hello", ["Hello!", "Hi there!"]],
+    ["how are you?", ["I'm a bot, I'm always good!"]],
+    ["what's your name?", ["I'm a simple chatbot!"]],
+]
+
+chatbot = Chat(pairs, reflections)
+chatbot.converse()
